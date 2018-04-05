12ab1236c642e7c82d54a4b49d72d65a6d912f15
karlinglee93 <459607025@qq.com>
2018-04-05 15:34:52 +0800

    tips for the last element in an array, which is different from the first one

---
+++ cpp_primer_plus/chapter_04/pointer_array_last_element.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+const int MAX = 3;
+
+int main ()
+{
+int  var[MAX] = {10, 100, 200};
+int  *ptr;
+
+// 指针中最后一个元素的地址
+ptr = &var[MAX-1];  // 和指针指向数组的第一个元素又不同
+// 可能因为数组中除了第一个元素以外，表示其他元素都应该这样做
+
+for (int i = MAX; i > 0; i--)
+{
+cout << "Address of var[" << i << "] = ";
+cout << ptr << endl;
+cout << "Value of var[" << i << "] = ";
+cout << *ptr << endl;
+
+// 移动到下一个位置
+ptr--;
+}
+
+return 0;
+}
+
