437ff13e7dffd7f5d130cdf36898030695cff019
karlinglee93 <459607025@qq.com>
2018-04-08 23:38:51 +0800

    some practices of chapter 6

---
+++ cpp_primer_plus/chapter_06/p_6_7.cpp
@@ -0,0 +1,36 @@
+// 看的答案
+
+#include <iostream>
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n_vowel = 0;
+    int n_consonant = 0;
+    int n_other = 0;
+    string str;
+    
+    cout << "Enter words (q to quit): " << endl;
+    
+    while(getline(cin, str))
+    {
+        if(str.length() == 1 && str[0] == 'q')
+            break;
+        if(! isalpha(str[0]))
+            n_other++;
+        else if(str[0] == 'a' || str[0] == 'e' || str[0] == 'i' || str[0] == 'o' || str[0] == 'u' ||
+                str[0] == 'A' || str[0] == 'E' || str[0] == 'I' || str[0] == 'O' || str[0] == 'U')
+            n_vowel++;
+        else
+            n_consonant++;
+    }
+    
+    cout << n_vowel << " words beginning with vowels" << endl;
+    cout << n_consonant << " words beginning with consonant" << endl;
+    cout << n_other << " others" << endl;
+    
+    return 0;
+}
